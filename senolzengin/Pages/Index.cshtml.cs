cd18fe683e5cc0890749847e12091ff4f254fcda
Şenol Zengin <senolzengin61@gmail.com>
2019-10-16 21:17:48 +0300

    add html template

--- senolzengin/Pages/Index.cshtml.cs
+++ senolzengin/Pages/Index.cshtml.cs
@@ -13,34 +13,37 @@ namespace senolzengin.Pages
 {
     public class IndexModel : PageModel
     {
-        public Item[] items;
-        
+        public Item items;
+
         private readonly IConfiguration _configuration;
 
         public IndexModel(IConfiguration configuration)
         {
             _configuration = configuration;
         }
+
         public class Item : SquidexEntityBase<_Item>
         {
         }
+
         public class _Item
         {
             public string Name { get; set; }
-            public string Surname{ get; set; }
-            public string Email{ get; set; }
-            public string Telephone{ get; set; }
+            public string Surname { get; set; }
+            public string Email { get; set; }
+            public string Telephone { get; set; }
             public string HighSchool { get; set; }
             public string University { get; set; }
             public string Title { get; set; }
             public string Departman { get; set; }
         }
+
         public async Task OnGet()
         {
             var manager = SquidexClientManager.FromOption(_configuration.ToSquidexOptions());
-            var client = manager.GetClient<Item,_Item>("information");
+            var client = manager.GetClient<Item, _Item>("information");
             var result = await client.GetAsync(context: SquidexParser.DefaultContext("en"));
-            var res = result.Items.ToArray();
+            var res = result.Items.ToArray().FirstOrDefault();
             items = res;
         }
     }
